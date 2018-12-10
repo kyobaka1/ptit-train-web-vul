70b50cf065c9605a432bfde4631ddfcc19d25d38
Đoàn Ngọc Vương <35132638+kyobaka1@users.noreply.github.com>
2018-12-10 17:29:59 +0700

    Update config.php

--- core/config/config.php
+++ core/config/config.php
@@ -1,8 +1,3 @@
 <?php 
-$config['install'] = '1';
-$config['user_root_me'] = 'N14DCAT138';
-$config['db_name'] = 'ptithcm_train';
-$config['db_user'] = 'root';
-$config['db_pass'] = '';
-$config['user_score'] = '25';
+
 ?>
