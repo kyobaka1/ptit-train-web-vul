697422a98e14fc9324187ca82e02305fe3a2df78
Đoàn Ngọc Vương <kiodark2@gmail.com>
2018-12-23 14:05:20 +0700

    Update

--- core/config/config.php
+++ core/config/config.php
@@ -1,8 +1,3 @@
 <?php 
-$config['install'] = '0';
-$config['user_root_me'] = 'VuongThieu';
-$config['db_name'] = 'ptithcm_train';
-$config['db_user'] = 'root';
-$config['db_pass'] = '';
-$config['user_score'] = '1725';
+
 ?>
